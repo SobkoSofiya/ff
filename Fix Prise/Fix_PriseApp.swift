b630d9e673da043f2ab9a77fa4b2591986a788e6
SobkoSofiya <sobkosofiya23@gmail.com>
2021-02-09 09:21:37 +0300

    Initial Commit

---
+++ Fix Prise/Fix_PriseApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fix_PriseApp.swift
+//  Fix Prise
+//
+//  Created by Sofi on 09.02.2021.
+//
+
+import SwiftUI
+
+@main
+struct Fix_PriseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
